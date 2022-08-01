81a79def8827539b811750357e94e21a4c0ce8a9
zakialbajili <zakyalbajili10@gmail.com>
2022-08-02 00:38:37 +0700

    menambahkan dua file

---
+++ kuliah/pertemuan9/hello.js
@@ -0,0 +1,30 @@
+
+//latihan objek
+function Angkot(sopir, trayek, penumpang, kas){
+    this.sopir=sopir;
+    this.trayek=trayek;
+    this.penumpang=penumpang;
+    this.kas=kas;
+    this.penumpangNaik=function(namaPenumpang){
+        this.penumpang.push(namaPenumpang);
+        return this.penumpang;
+    }
+    this.penumpangTurun = function(namaPenumpang, bayar){
+        if(this.penumpang.length === 0){
+            alert("Belum ada penumpang!");
+            return false;
+        }
+        else{
+            for (let i=0; i< this.penumpang.length; i++){
+                if (this.penumpang[i] === namaPenumpang){
+                    this.penumpang[i]=undefined;
+                    this.kas += bayar;
+                    return this.penumpang;
+                }
+            }
+        }
+    }
+
+}
+
+var angkot1= new Angkot("Zaky",["Balaraja","Serang"],[],0);
